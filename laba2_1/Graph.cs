4e1249641d458dde6fd6c32df2a258a9879a0e5e
ketaterina <73121693+ketaterina@users.noreply.github.com>
2020-10-20 15:42:40 +0500

    Add files via upload

---
+++ laba2_1/Graph.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba2_1
+{
+    public partial class Graph : Form
+    {
+        public Graph(string surname)
+        {
+            InitializeComponent();
+            double starting = 0, ending = 10, h = 0.1, x = starting, y;
+            this.chart1.Series[0].Points.Clear();
+            chart1.ChartAreas[0].AxisX.IntervalOffset = 0;
+            chart1.ChartAreas[0].AxisY.IntervalOffset = 0;
+
+            while (x <= ending)
+            {
+                y = Program.TheFunc(surname, x);
+                this.chart1.Series[0].Points.AddXY(x, y);
+                x += h;
+            }
+        }
+
+  
+    }
+}
